da3956c553021df6a58ce01aaa4c317d6990ef61
smallcats <gschnei24@gmail.com>
2017-04-27 10:34:53 -0400

    Changes to be committed: 	modified:   ReadMe.pdf 	modified:   ReadMe.tex 	modified:   lrTrain.m 	new file:   theta_im_randinit.png 	new file:   theta_im_zeroinit.png

--- lrTrain.m
+++ lrTrain.m
@@ -1,7 +1,8 @@
 function theta = lrTrain(X,y,lambda,theta_init)
   #Trains logistic regression by minimizing lrCost on training set X with labels
   #y with regularization parameter lambda and starting with theta_init as 
-  #initial model parameters. Uses fminunc to minimize.
+  #initial model parameters. The first entry of theta(_init) is the bias term.
+  #Uses fminunc to minimize.
   
   #size(X) = [m,n], size(y) = [m,1], size(theta_init) = [n+1,1]
   
