2dc2be598d4bee0a2c821a625208c513ec144041
smallcats <gschnei24@gmail.com>
2017-05-04 15:27:52 -0400

    Fixes to perceptron

--- perceptron.jl
+++ perceptron.jl
@@ -19,7 +19,7 @@ function pcpttrainstep(x,y,winit=zeros(size(x,2)+1,length(y)))
 
   classification = pcpteval(x,winit) .> 0
   correct = classification .== y
-  (winit + transpose(hcat(1,x))*(correct .* (-2*classification + 1)), correct)
+  (winit + transpose(hcat(1,x))*(~correct .* (-2*classification + 1)), correct)
 end
 
 function arrayand(arr)
@@ -31,7 +31,7 @@ function arrayand(arr)
   out
 end
 
-function pcpttrain(X,Y,winit=zeros(size(X,2)+1, size(Y,1))
+function pcpttrain(X,Y,winit=zeros(size(X,2)+1, size(Y,2)),maxstep=10000)
   #trains size(Y,2) perceptrons on size(X,1) samples in parallel.
 
   #size(X) = (m,n), size(Y) = (m,p), size(winit) = (n+1,p)
@@ -39,15 +39,19 @@ function pcpttrain(X,Y,winit=zeros(size(X,2)+1, size(Y,1))
 
   (m,p) = size(Y)
   w = winit
+  #println("w: $(size(w))")
+  #println("X: $(size(X))")
+  #println("Y: $(size(Y))")
   steps = 0
   correct = fill(false, (m,p))
   sep = false
   while !sep
     sampnum = steps%m+1
-    printf("\rstep: $steps")
-    w,correct[sampnum,:]  = pcpttrainstep(X[sampnum,:],Y[sampnum,:],w)
-    if steps > 10000 break end
+    print("\rstep: $steps")
+    w,correct[sampnum,:]  = pcpttrainstep(X[[sampnum],:],Y[[sampnum],:],w)
+    if steps > maxstep break end
     sep = arrayand(correct)
+    steps += 1
   end
-  w
+  (w,correct)
 end
