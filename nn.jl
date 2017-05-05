cbca8f02241731ac3610393f03151e7202381e28
smallcats <gschnei24@gmail.com>
2017-05-05 19:15:13 -0400

    Minor changes to docs and added backprop functions

--- nn.jl
+++ nn.jl
@@ -3,7 +3,7 @@ Forward propagate m samples with activation function act, weights w and features
 act is a function Float64 -> Float64 that acts coordinate-wise on arrays, size(w) = (n+1,p) (bias weights in the first row), size(X) = (m,n).
 m is the number of samples, n is the size of the input layer, p is the size of the output layer.
 """
-function fprop(X,w,act)
+function fproplayer(X,w,act)
   Xbias = hcat(ones(size(X,1)),X)
   act(Xbias*w)
 end
@@ -35,11 +35,24 @@ end
 Convolutional forward propagation with activation function act, convolution kernel k and features X.
 at is a function Float64 -> Float64 that acts coordinate-wise on arrays, k the matrix giving the kernel of convolution and X is the feature matrix (so, 2D data).
 """
-function convfprop(X,k,act)
+function convfproplayer(X,k,act)
   csize = size(k,1)
   nextlayer = zeros(size(X,1)-csize+1, size(X,2)-csize+1)
   for i = 1:csize
     nextlayer += X[i:end-csize+i,:]*thickdiag(k[i,:], size(X,1))
   end
   act(nextlayer)
+end
+
+"""
+Given derivatives of the next layer's output after activation wrt its values before activation (der),
+  backpropagated values for the next layer (bv),
+  and outgoing weights without bias weights (w)
+  returns the backpropagated values for the current layer.
+
+size(dernext) = (1,l), size(bvnext) = (1,l), size(w) = (n,l) 
+  where l is the size of the next layer and n is the size of the current layer.
+"""
+function bproplayer(dernext, bvnext, w)
+  (dernext.*bvnext)*w'
 end
